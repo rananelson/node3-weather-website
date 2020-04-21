644e3d955a24113b280fe6e1cf3a81c1b7cafe55
nelson rana <nelson.rana@bankaigroup.com>
2020-04-21 11:15:36 +0530

    added uv index

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,9 +8,9 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
-        }
+        } else {            
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain. And UV index is '+  body.currently.uvIndex) 
+        } 
     })
 }
 
